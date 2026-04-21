2683bcb1a0aaa08ab1f0db6e81bccc97c38ed382
rjrcor13 <56808455+rjrcor13@users.noreply.github.com>
2026-04-21 14:09:51 +0800

    test

--- app/api/outstatic/[[...ost]]/route.js
+++ app/api/outstatic/[[...ost]]/route.js
@@ -1,22 +1,4 @@
-// import { OutstaticApi } from 'outstatic';
+import { OutstaticApi } from 'outstatic'
 
-// export const GET = OutstaticApi.GET;
-// export const POST = OutstaticApi.POST;
-// import { Outstatic } from "outstatic";
-
-// export async function GET(req) {
-//     const handler = Outstatic();
-//     return handler.GET(req);
-// }
-
-// export async function POST(req) {
-//     const handler = Outstatic();
-//     return handler.POST(req);
-// }
-
-import { Outstatic } from "outstatic";
-
-const outstatic = Outstatic();
-
-export const GET = outstatic;
-export const POST = outstatic;
+export const GET = OutstaticApi.GET
+export const POST = OutstaticApi.POST
